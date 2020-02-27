f27519eda4882e06062c91e86fd023335eff04cf
JoeTakasuna <jonpili99@gmail.com>
2020-02-27 15:55:59 +0900

    テスト表示

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,11 +1,33 @@
 <template lang="pug">
   .container
     h1 Top
+    vue-apex-charts(width="500" type="bar" :options="options" :series="series")
 </template>
 
 <script>
+import VueApexCharts from 'vue-apexcharts'
+
 export default {
-  name: 'Top'
+  name: 'Top',
+  components: {
+    VueApexCharts
+  },
+  data () {
+    return {
+      options: {
+        chart: {
+          id: 'vuechart-example'
+        },
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }]
+    }
+  }
 };
 </script>
 
